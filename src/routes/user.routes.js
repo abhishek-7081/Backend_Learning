e7a11fd9b0d2c34a6a57d9e2f8ff3c43ac8c0d04
abhishek-7081 <luckydwivedi7081@gmail.com>
2024-12-31 09:04:48 +0530

    added refresh token code

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {loginUser, 
     logoutUser, 
-    registerUser } from "../controllers/user.controller.js";
+    registerUser ,
+    refreshAccessToken
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -26,6 +28,7 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 export default router
